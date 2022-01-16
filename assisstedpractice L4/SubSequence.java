47541bbe032cf54a68bfe3359e78d77efb85679a
csadhana <97276140+csadhana@users.noreply.github.com>
2022-01-16 16:57:05 +0530

    Lesson 4 code

---
+++ assisstedpractice L4/SubSequence.java
@@ -0,0 +1,33 @@
+package com.sl.l4.assistedpractice;
+
+public class SubSequence {
+	static int max_ref;
+	
+	static  int _list(int arr[], int n)
+	{
+		if(n==1)
+			return 1;
+		int result,max_ending = 1;
+		for(int i=1;i<n;i++)
+		{
+			result = _list(arr,i);
+		    if(arr[i-1]<arr[n-1] && result+1 >max_ending)
+		    	max_ending = result+1;
+		}
+		if(max_ref < max_ending)
+			max_ref = max_ending;
+		return max_ending;
+	}
+	static int list(int n,int arr[])
+	{
+		max_ref =1;
+		_list(arr,n);
+		return max_ref;
+	}
+	public static void main(String[] args) {
+		int arr[] = {30,55,69,83,10,36,90,3,28};
+		int n = arr.length;
+		System.out.println("Length of list is : " + _list(arr,n) + "\n");
+	}
+
+}
---
+++ assisstedpractice L4/package-info.java
@@ -0,0 +1,1 @@
+package com.sl.l4.assistedpractice;
